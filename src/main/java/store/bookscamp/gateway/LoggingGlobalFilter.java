18e3d069bba23bd105961bd77b8aea386fbe52c1
BaekSungJun <por9535@gmail.com>
2025-10-27 15:30:43 +0900

    log()로 보기 yml설정 추가(CORS)

--- src/main/java/store/bookscamp/gateway/LoggingGlobalFilter.java
+++ src/main/java/store/bookscamp/gateway/LoggingGlobalFilter.java
@@ -1,19 +1,23 @@
 package store.bookscamp.gateway;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
+@Slf4j
 @Component
 public class LoggingGlobalFilter implements GlobalFilter {
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
 
+
         String method = exchange.getRequest().getMethod().name();
         String requestPath = exchange.getRequest().getURI().toString();
+        log.debug(method + " request = " +requestPath);
 
 
         return chain.filter(exchange).then(Mono.fromRunnable(() -> {
