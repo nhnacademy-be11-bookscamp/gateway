8c3b5441f046d77ebc7d145f6cd73e1c395f13ed
SungJunBaek <63231250+BaekSeongJun@users.noreply.github.com>
2025-11-06 16:03:08 +0900

    Develop (#18)
    * LoggingGlobalFilter 추가,
    porm및 yml 파일 수정
    bookcamp 디렉토리명 -> bookscamp로 수정
    
    * Lombok의존성 추가
    
    * fix: yml, pom webflux 형식으로 수정 (#5)
    
    * Add pull request template to repository
    
    * Create pull_request_template.md
    
    Add a pull request template for better PR management.
    
    * 오타난 파일 삭제
    
    * log()로 보기
    yml설정 추가(CORS)
    
    * log()로 보기 (#6)
    
    yml설정 추가(CORS)
    
    * yml (#8)
    
    * Feature/yml (#10)
    
    * yml
    
    * yml
    
    * feat: gateway에서 토큰 decoding 추가
    
    * [Feature/jwt] jwt 토큰 관련 기능 추가 (#15)
    
    * Develop (#11)
    
    * LoggingGlobalFilter 추가,
    porm및 yml 파일 수정
    bookcamp 디렉토리명 -> bookscamp로 수정
    
    * Lombok의존성 추가
    
    * fix: yml, pom webflux 형식으로 수정 (#5)
    
    * Add pull request template to repository
    
    * Create pull_request_template.md
    
    Add a pull request template for better PR management.
    
    * 오타난 파일 삭제
    
    * log()로 보기
    yml설정 추가(CORS)
    
    * log()로 보기 (#6)
    
    yml설정 추가(CORS)
    
    * yml (#8)
    
    * Feature/yml (#10)
    
    * yml
    
    * yml
    
    ---------
    
    Co-authored-by: Cherrrryk <kimcherry01@naver.com>
    
    * Feature/yml dev prod (#12)
    
    * LoggingGlobalFilter 추가,
    porm및 yml 파일 수정
    bookcamp 디렉토리명 -> bookscamp로 수정
    
    * Lombok의존성 추가
    
    * fix: yml, pom webflux 형식으로 수정 (#5)
    
    * Add pull request template to repository
    
    * Create pull_request_template.md
    
    Add a pull request template for better PR management.
    
    * 오타난 파일 삭제
    
    * log()로 보기
    yml설정 추가(CORS)
    
    * log()로 보기 (#6)
    
    yml설정 추가(CORS)
    
    * yml (#8)
    
    * Feature/yml (#10)
    
    * yml
    
    * yml
    
    * gateway yml 파일 환경 분리
    
    ---------
    
    Co-authored-by: BaekSungJun <por9535@gmail.com>
    Co-authored-by: SungJunBaek <63231250+BaekSeongJun@users.noreply.github.com>
    Co-authored-by: Cherrrryk <kimcherry01@naver.com>
    
    * Develop (#13)
    
    * LoggingGlobalFilter 추가,
    porm및 yml 파일 수정
    bookcamp 디렉토리명 -> bookscamp로 수정
    
    * Lombok의존성 추가
    
    * fix: yml, pom webflux 형식으로 수정 (#5)
    
    * Add pull request template to repository
    
    * Create pull_request_template.md
    
    Add a pull request template for better PR management.
    
    * 오타난 파일 삭제
    
    * log()로 보기
    yml설정 추가(CORS)
    
    * log()로 보기 (#6)
    
    yml설정 추가(CORS)
    
    * yml (#8)
    
    * Feature/yml (#10)
    
    * yml
    
    * yml
    
    ---------
    
    Co-authored-by: BaekSungJun <por9535@gmail.com>
    Co-authored-by: SungJunBaek <63231250+BaekSeongJun@users.noreply.github.com>
    Co-authored-by: Cherrrryk <kimcherry01@naver.com>
    
    * Feature/book (#14)
    
    * LoggingGlobalFilter 추가,
    porm및 yml 파일 수정
    bookcamp 디렉토리명 -> bookscamp로 수정
    
    * Lombok의존성 추가
    
    * fix: yml, pom webflux 형식으로 수정 (#5)
    
    * Add pull request template to repository
    
    * Create pull_request_template.md
    
    Add a pull request template for better PR management.
    
    * 오타난 파일 삭제
    
    * log()로 보기
    yml설정 추가(CORS)
    
    * log()로 보기 (#6)
    
    yml설정 추가(CORS)
    
    * yml (#8)
    
    * Feature/yml (#10)
    
    * yml
    
    * yml
    
    * fix:도서 이미지 보낼 때 multipart 위치 변경
    
    ---------
    
    Co-authored-by: BaekSungJun <por9535@gmail.com>
    Co-authored-by: SungJunBaek <63231250+BaekSeongJun@users.noreply.github.com>
    
    * feat: gateway에서 토큰 decoding 추가
    
    ---------
    
    Co-authored-by: Cherrrryk <kimcherry01@naver.com>
    Co-authored-by: 2Limit <109938169+2Limit@users.noreply.github.com>
    
    * Update AuthorizationFilter.java
    
    * Fix typo in excluded paths for authorization filter
    
    * [Feature/url] url추가 (#17)
    
    * yml
    
    * yml
    
    * feat:gateway url추가
    
    ---------
    
    Co-authored-by: Cherrrryk <kimcherry01@naver.com>
    Co-authored-by: 2Limit <109938169+2Limit@users.noreply.github.com>
    Co-authored-by: kiju-dev <kijujeong.dev@gmail.com>

---
+++ src/main/java/store/bookscamp/gateway/AuthorizationFilter.java
@@ -0,0 +1,77 @@
+package store.bookscamp.gateway;
+
+import io.jsonwebtoken.Claims;
+import lombok.RequiredArgsConstructor;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class AuthorizationFilter implements GlobalFilter, Ordered {
+
+    private final JwtTokenProvider jwtTokenProvider;
+    private static final List<String> EXCLUDED_PATHS = List.of(
+            "/auth-server/login",
+            "/api-server/sign-up",
+            "/public",
+            "/login",
+            "/api-server/member/check-id", 
+            "/api-server/categories",
+            "/api-server/books/like",
+            "/api-server/admin/category"
+    );
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        ServerHttpRequest request = exchange.getRequest();
+        String path = request.getURI().getPath();
+
+        if (EXCLUDED_PATHS.stream().anyMatch(path::contains)) {
+            return chain.filter(exchange);
+        }
+
+        String authHeader = request.getHeaders().getFirst("Authorization");
+
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            return onError(exchange, "토큰이 없거나 형식이 잘못되었습니다.", HttpStatus.UNAUTHORIZED);
+        }
+
+        String jwtToken = authHeader.substring(7);
+
+        try {
+            Claims claims = jwtTokenProvider.getClaims(jwtToken);
+            Long memberId = claims.get("memberId", Long.class);
+            String role = claims.get("role", String.class);
+
+            ServerHttpRequest authorizedRequest = request.mutate()
+                    .header("X-User-ID", String.valueOf(memberId))
+                    .header("X-User-Role", role)
+                    .build();
+
+            return chain.filter(exchange.mutate().request(authorizedRequest).build());
+
+        } catch (Exception e) {
+            return onError(exchange, "JWT 토큰이 유효하지 않습니다.", HttpStatus.UNAUTHORIZED);
+        }
+    }
+
+    private Mono<Void> onError(ServerWebExchange exchange, String err, HttpStatus httpStatus) {
+        ServerHttpResponse response = exchange.getResponse();
+        response.setStatusCode(httpStatus);
+
+        return response.writeWith(Mono.just(response.bufferFactory().wrap(err.getBytes())));
+    }
+
+    @Override
+    public int getOrder() {
+        return -1;
+    }
+}
---
+++ src/main/java/store/bookscamp/gateway/JwtTokenProvider.java
@@ -0,0 +1,29 @@
+package store.bookscamp.gateway;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import java.nio.charset.StandardCharsets;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+
+@Component
+public class JwtTokenProvider {
+
+    private final Key key;
+
+    public JwtTokenProvider(@Value("${spring.jwt.secret}") String secretKey) {
+        byte[] keyBytes = secretKey.getBytes(StandardCharsets.UTF_8);
+        this.key = Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public Claims getClaims(String token) {
+        return Jwts.parser()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+}
